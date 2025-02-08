766dd30d061a0d1d3675cee31cc26916734cea7f
mayu-code <durbulemayu@gmail.com>
2025-02-08 12:19:11 +0530

    sync

--- src/main/java/com/transaction/book/controller/UserController.java
+++ src/main/java/com/transaction/book/controller/UserController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.transaction.book.dto.responseDTO.Dashboard;
 import com.transaction.book.dto.responseObjects.DataResponse;
 import com.transaction.book.dto.responseObjects.SuccessResponse;
+import com.transaction.book.services.serviceImpl.CustomerServiceImpl;
 import com.transaction.book.services.serviceImpl.UserServiceImpl;
 
 @RestController
@@ -23,12 +25,38 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
+    @Autowired
+    private CustomerServiceImpl customerServiceImpl;
+
     @GetMapping("/getProfile")
     public ResponseEntity<?> getProfile(@RequestHeader("Authorization")String jwt){
         try{
             DataResponse response = new DataResponse();
             response.setData(this.userServiceImpl.getUserByJwt(jwt));
             response.setMessage("User profile get successfully !");
+            response.setHttpStatus(HttpStatus.OK);
+            response.setStatusCode(200);
+            return ResponseEntity.of(Optional.of(response));
+
+        }catch(Exception e){
+            SuccessResponse response = new SuccessResponse();
+            response.setMessage(e.getMessage());
+            response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.setStatusCode(500);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping("/getDashboard")
+    public ResponseEntity<?> getDashboard(){
+        try{
+            Dashboard dashboard = new Dashboard();
+            dashboard.setYouWillGate(this.customerServiceImpl.getTotalGetAmount());
+            dashboard.setYouWillGave(this.customerServiceImpl.getToalGaveAmount());
+
+            DataResponse response = new DataResponse();
+            response.setData(dashboard);
+            response.setMessage("Dashboard get successfully !");
             response.setHttpStatus(HttpStatus.OK);
             response.setStatusCode(200);
             return ResponseEntity.of(Optional.of(response));
