0dcf3dd4ccb81165bf3b54227bbad9577d0429c9
mayu-code <durbulemayu@gmail.com>
2025-02-10 12:29:53 +0530

    sync

--- src/main/java/com/transaction/book/repository/CustomerRepo.java
+++ src/main/java/com/transaction/book/repository/CustomerRepo.java
@@ -10,29 +10,38 @@ import com.transaction.book.dto.responseDTO.CusotomerFullResponse;
 import com.transaction.book.dto.responseDTO.CustomerResponse;
 import com.transaction.book.entities.Customer;
 
-public interface CustomerRepo extends JpaRepository<Customer,Long>{
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
     Customer findByMobileNo(String mobileNo);
 
     @Query("SELECT c FROM Customer c ORDER BY c.updateDate DESC")
     List<Customer> findAllCustomers();
-    
+
     @Query("SELECT SUM(c.amount) FROM Customer c WHERE c.amount<0")
     Double getTotalGetAmount();
-    
+
     @Query("SELECT SUM(c.amount) FROM Customer c WHERE c.amount>0")
     Double getTotalGaveAmount();
 
     @Query("""
-        SELECT new com.transaction.book.dto.responseDTO.CustomerResponse(c.id, c.name, c.mobileNo, c.gstinNo, c.amount, c.dueDate, c.updateDate) 
-        FROM Customer c
-        WHERE (:query IS NULL OR (c.name LIKE %:query% OR c.mobileNo LIKE %:query%))
-        AND ((:gave = true OR c.amount > 0) OR (:get = true OR c.amount < 0) OR (:settel =true OR c.amount=0))
-        ORDER BY c.updateDate DESC
-    """)
-    List<CustomerResponse> findAllCustomerResponse(@Param("query")String query,@Param("gave")boolean gave,@Param("get")boolean get,@Param("settel")boolean settel);
+                SELECT new com.transaction.book.dto.responseDTO.CustomerResponse(
+                    c.id, c.name, c.mobileNo, c.gstinNo, c.amount, c.dueDate, c.updateDate)
+                FROM Customer c
+                WHERE (:query IS NULL OR (c.name LIKE %:query% OR c.mobileNo LIKE %:query%))
+                AND (
+                    (:gave = true AND c.amount > 0) OR
+                    (:get = true AND c.amount < 0) OR
+                    (:settel = true AND c.amount = 0) OR
+                    (:gave = false AND :get = false AND :settel = false)  
+                )
+                ORDER BY c.updateDate DESC
+            """)
+    List<CustomerResponse> findAllCustomerResponse(@Param("query") String query,
+            @Param("gave") boolean gave,
+            @Param("get") boolean get,
+            @Param("settel") boolean settel);
 
     @Query("SELECT new com.transaction.book.dto.responseDTO.CusotomerFullResponse(c.id, c.name, c.mobileNo, c.gstinNo, c.amount, c.dueDate, c.updateDate, c.address) FROM Customer c WHERE c.id = :id")
     CusotomerFullResponse findCustomerResponseById(@Param("id") long id);
-   
+
 }
